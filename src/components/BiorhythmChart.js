0d2bb39f9cd6096c20ff4674d6477d8df5c47a66
sureshkumaranbu <sureshkumar.anbu@gmail.com>
2023-01-02 13:32:35 -0500

    commit 2

--- src/components/BiorhythmChart.js
+++ src/components/BiorhythmChart.js
@@ -1,10 +1,5 @@
 import {LineChart, ResponsiveContainer, Line} from 'recharts';
 import { calculateBiorhythmsSeries } from '../lib/biorhythms';
-const data = [
-    { date: '2022-10-01', physical: 1.0, emotional: -0.5, intellectual: -1.5},
-    {date: '2022-10-02', physical: 1.0, emotional: -1, intellectual: -1.0},
-    {date: '2022-10-03', physical: 1.0, emotional: 1, intellectual: 1.0},
-];
 
 function BiorhythmChart({birthDate, targetBate}) {
     const series = calculateBiorhythmsSeries(birthDate, targetBate, 3);
